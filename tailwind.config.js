ed584039171d528a241bacb3b74a8fdddafd54fe
P3ntest <juliusvanvoorden@outlook.com>
2021-12-07 23:57:47 +0100

    major progress

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: "#000",
+        white: {
+          smoke: "#ECECEC",
+          DEFAULT: "#fff",
+          light: "#878a8c",
+        },
+        main: {
+          darker: "#12121c",
+          dark: "#232629",
+          DEFAULT: "#1e1e2d",
+          light: "#36393d",
+        },
+        val: {
+          DEFAULT: "#f94655",
+        },
+        text: {
+          nav: "#8FA3B0",
+          card: "#0F1923",
+          subcard: "#4F4245",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
