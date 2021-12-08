c7c149a52a6541dab72848f5293abba1a802f927
P3ntest <juliusvanvoorden@outlook.com>
2021-12-08 22:28:03 +0100

    fix bug

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 import { minifyHtml } from "vite-plugin-html";
 
 export default defineConfig({
-  plugins: [minifyHtml()],
+  //   plugins: [minifyHtml()],
 });
